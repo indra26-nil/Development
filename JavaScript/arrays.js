513fc9e210303fef4dcfa38acccefb4e075d7c9b
indra26 nil <indranillaha4@gmail.com>
2026-05-04 22:04:34 +0530

    Upto Events

---
+++ JavaScript/arrays.js
@@ -0,0 +1,28 @@
+let arr = [1,2,3,5,6,7,8,9]
+arr.forEach(element => {
+   console.log(element+100);
+   
+    
+});
+
+// Map use case in array to create new array for same elements size 
+
+let brr = arr.map(function(val){
+    val*=5
+    return val;
+})
+
+console.log(brr);
+
+
+// Filter use case 
+
+let n = arr.filter(function(val){
+
+    if(val<8&&val>3){
+        return true;
+    }
+
+})
+
+console.log(n);
---
+++ JavaScript/objects.js
@@ -0,0 +1,16 @@
+let obj = {
+    name : "Indranil",
+    location : "Kolkata",
+    pin : 712203,
+    edu : {
+        clg : "HETC",
+        sem : 4,
+        yr : 2
+    }
+}
+obj.name="Nil"
+obj.edu.sem=0
+console.log(obj);
+
+
+
